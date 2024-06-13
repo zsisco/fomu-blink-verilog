af212dabc8c8cf2e035708be01412c3cc44bf6d7
Zach Sisco <zach@zsisco.net>
2024-06-12 21:28:58 -0700

    change counter back to original config

--- counter.py
+++ counter.py
@@ -3,8 +3,8 @@ import pyrtl
 # Use counter logic to divide system clock.
 # The clock is 48 MHz, so we divide it down by 2^28.
 
-counter_o = pyrtl.Output(bitwidth=58, name='counter_o')
-counter = pyrtl.Register(bitwidth=58, name='counter')
+counter_o = pyrtl.Output(bitwidth=29, name='counter_o')
+counter = pyrtl.Register(bitwidth=29, name='counter')
 
 counter.next <<= counter + 1
 counter_o <<= counter
